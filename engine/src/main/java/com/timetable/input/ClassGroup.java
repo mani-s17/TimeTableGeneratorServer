16e7da808bcf41d19c38d2f4acca3574968bf163
prembhaskal <pbhaskal@gmail.com>
2014-03-28 23:49:34 +0530

    Completed modeling the input class required by the time table generation engine.

--- engine/src/main/java/com/timetable/input/ClassGroup.java
+++ engine/src/main/java/com/timetable/input/ClassGroup.java
@@ -1,7 +1,38 @@
 package com.timetable.input;
 
+/**
+ * A class group is a combination of students (of a standard) who study a set of common subjects.
+ *
+ */
 public class ClassGroup {
+
 	private int classStandard;
+
 	private int totalStudents;
+
 	private String[] subjects;
+
+	public int getClassStandard() {
+		return classStandard;
+	}
+
+	public void setClassStandard(int classStandard) {
+		this.classStandard = classStandard;
+	}
+
+	public int getTotalStudents() {
+		return totalStudents;
+	}
+
+	public void setTotalStudents(int totalStudents) {
+		this.totalStudents = totalStudents;
+	}
+
+	public String[] getSubjects() {
+		return subjects;
+	}
+
+	public void setSubjects(String[] subjects) {
+		this.subjects = subjects;
+	}
 }
--- engine/src/main/java/com/timetable/input/TimeTableInput.java
+++ engine/src/main/java/com/timetable/input/TimeTableInput.java
@@ -1,7 +1,37 @@
 package com.timetable.input;
 
+/**
+ * TimeTableInput represents the total information required to generate the weekly timetable.
+ * It has the information of the teachers and the class groups (learners).
+ */
 public class TimeTableInput {
 	private int totalPeriods;
+
 	private TeacherInfo[] teacherInfos;
+
 	private ClassGroup[] classGroups;
+
+	public int getTotalPeriods() {
+		return totalPeriods;
+	}
+
+	public void setTotalPeriods(int totalPeriods) {
+		this.totalPeriods = totalPeriods;
+	}
+
+	public TeacherInfo[] getTeacherInfos() {
+		return teacherInfos;
+	}
+
+	public void setTeacherInfos(TeacherInfo[] teacherInfos) {
+		this.teacherInfos = teacherInfos;
+	}
+
+	public ClassGroup[] getClassGroups() {
+		return classGroups;
+	}
+
+	public void setClassGroups(ClassGroup[] classGroups) {
+		this.classGroups = classGroups;
+	}
 }
