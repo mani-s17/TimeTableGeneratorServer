a63dadd7d0930683d3ee6a249c7e1eaa5e7c19f1
Subramaniam S <subramaniam.s17@gmail.com>
2014-04-05 23:29:42 +0530

    Just adding Test files, will cover test cases in a while.

---
+++ engine/src/test/java/com/timetable/server/engine/generator/BruteForceGeneratorTest.java
@@ -0,0 +1,11 @@
+package com.timetable.server.engine.generator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Subramaniam S
+ * Date: 4/5/2014
+ * Time: 11:28 PM
+ */
+public class BruteForceGeneratorTest
+{
+}
